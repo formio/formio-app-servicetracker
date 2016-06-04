7f8b1e11d8869007eeed35c7e276500521e46ddb
Travis Tidwell <travis@form.io>
2016-06-03 23:21:05 -0500

    Updating build.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -11,26 +11,29 @@
       'formio',
       'ngFormioHelper'
     ])
-    .factory('Geolocation', ['$q', '$window', function($q, $window) {
-      return {
-        getCurrentPosition: function() {
-          var deferred = $q.defer();
+    .factory('Geolocation', Geolocation);
 
-          if (!$window.navigator.geolocation) {
-            return deferred.reject('Geolocation not supported.');
-          }
+  /** @ngInject */
+  function Geolocation($q, $window) {
+    return {
+      getCurrentPosition: function() {
+        var deferred = $q.defer();
 
-          $window.navigator.geolocation.getCurrentPosition(
-            function(position) {
-              deferred.resolve(position);
-            },
-            function(err) {
-              deferred.reject(err);
-            }
-          );
-
-          return deferred.promise;
+        if (!$window.navigator.geolocation) {
+          return deferred.reject('Geolocation not supported.');
         }
-      };
-    }]);
+
+        $window.navigator.geolocation.getCurrentPosition(
+          function(position) {
+            deferred.resolve(position);
+          },
+          function(err) {
+            deferred.reject(err);
+          }
+        );
+
+        return deferred.promise;
+      }
+    };
+  }
 })();
